a345d0c89f21792c1924f0d6c18af263565d9405
YEOJU-SEOK <yeoju.ac@gmail.com>
2022-04-12 00:09:29 +0900

    create

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `          
@@ -13,6 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -63,7 +65,45 @@ var app = http.createServer(function(request,response){
           });
         });
       }
-    } else {
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else if(pathname === '/create_process'){
+      var body;
+
+      request.on('data', function(data){
+        body = body + data;
+
+      })
+      request.on('end', function(){
+        // var post = qs.parse(body);
+        // console.log(post.description);
+        var title = new URLSearchParams(body).get('undefinedtitle');
+        var description = new URLSearchParams(body).get('description');
+        // 파일 저장함
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location:`/?id=${title}`});
+          response.end('Success');
+        })
+      })
+
+
+    }else {
       response.writeHead(404);
       response.end('Not found');
     }
